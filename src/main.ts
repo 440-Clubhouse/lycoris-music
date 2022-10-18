d3e4b5df963db2552b68823faafb684d5729e973
Daniel Ding <41363844+BioniCosmos@users.noreply.github.com>
2022-10-18 19:21:44 +0800

    Add Tailwind CSS and Tailwind Elements

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,11 @@
-import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
+import 'tw-elements'
+import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-
-import './assets/main.css'
+import './style.css'
 
 const app = createApp(App)
-
 app.use(createPinia())
 app.use(router)
-
 app.mount('#app')
